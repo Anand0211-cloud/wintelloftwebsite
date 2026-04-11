26e38c8ac0ec3b7ab69bd586a76fa9136f62b4fa
Anand0211-cloud <gonyanand@gmail.com>
2026-04-12 02:55:03 +0530

    dep 7

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -114,11 +114,16 @@ const Contact = () => {
             </section>
 
             <section className="map-section">
-                {/* Placeholder Map */}
-                <div className="map-placeholder">
-                    <MapPin size={48} className="text-accent" />
-                    <p>Map Location Placeholder</p>
-                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6860.933849224265!2d76.73210013406452!3d30.705271361182213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fec254207d7d3%3A0x440eef5f4be26f01!2sSector%2051%2C%20Chandigarh!5e0!3m2!1sen!2sin!4v1775942572808!5m2!1sen!2sin"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0, borderRadius: '0' }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="Intelloft Office Location"
+                ></iframe>
             </section>
         </main>
     );
